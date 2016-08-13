0fa4ec9bee1e7127c36f38f3b4413471244386bf
Kristina Chodorow <k.chodorow@gmail.com>
2016-08-13 14:44:27 -0400

    Initial commit

---
+++ app/login.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"net/http"
+	"html/template"
+	"path"
+
+	"appengine"
+	"appengine/user"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+        }
+
+	fp := path.Join("static", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, u); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
