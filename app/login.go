b37d662f06937ba1c4c4793042898b16d156bec5
Kristina Chodorow <k.chodorow@gmail.com>
2016-08-13 15:07:54 -0400

    Add login/logout functionality

--- app/login.go
+++ app/login.go
@@ -10,10 +10,32 @@ import (
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/signup", loginHandler)
+	http.HandleFunc("/logout", logoutHandler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func writeIndex(w http.ResponseWriter, u *user.User) {
+	fp := path.Join("static", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, u); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	writeIndex(w, u);
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	u := user.Current(c)
 	if u == nil {
@@ -26,15 +48,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusFound)
 		return
         }
+	writeIndex(w, u)
+}
 
-	fp := path.Join("static", "index.html")
-	tmpl, err := template.ParseFiles(fp)
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		w.Header().Set("Location", "/")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	url, err := user.LogoutURL(c, r.URL.String())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	if err := tmpl.Execute(w, u); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	w.Header().Set("Location", url)
+	w.WriteHeader(http.StatusFound)
 }
